b0b9b0d57aad131e585385bbdef5bbdd5de2a1a0
srph <seraphipod@gmail.com>
2015-12-02 17:52:18 +0800

    Add note for koa-static workaround.

--- server/middlewares/koaStatic.js
+++ server/middlewares/koaStatic.js
@@ -3,4 +3,6 @@ import path from 'path';
 import serve from 'koa-static';
 
 const build = path.resolve(__dirname, '../../public');
+// Set `index` / default page to `yolo.html` so
+// `index.html` isn't initially served for server-side rendering
 export default serve(build, { index: 'yolo.html' });

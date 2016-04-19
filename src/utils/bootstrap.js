5bc2cb4b5996a7a2affc33e41e22d1243d533042
Kier Borromeo <seraphipod@gmail.com>
2016-04-19 14:03:04 +0800

    bootstrap: add global rejected promise handler

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -2,6 +2,10 @@
 
 if ( process.env.NODE_ENV !== 'production' ) {
   require('axios-response-logger');
+  
+  global.onunhandledrejection = function({reason}) {
+    console.error(reason, reason.stack);
+  }
 }
 
 require('axios-base-url')(process.env.API);

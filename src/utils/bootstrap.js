d8e89bc66e86f6b2a251b367122322dea73e4161
srph <seraphipod@gmail.com>
2016-04-20 13:03:08 +0800

    remove axios-response-logger

--- src/utils/bootstrap.js
+++ src/utils/bootstrap.js
@@ -1,8 +1,6 @@
 // Import all utils that needs to be initialized
 
 if ( process.env.NODE_ENV !== 'production' ) {
-  require('axios-response-logger');
-  
   global.onunhandledrejection = function({reason}) {
     console.error(reason, reason.stack);
   }

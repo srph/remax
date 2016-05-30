0dcba2ecde7e062a9b859d8004dca86538ce7a82
srph <seraphipod@gmail.com>
2016-05-30 17:56:33 +0800

    Use stored history instance

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import 'babel-polyfill';
 import './utils/bootstrap';
 import React from 'react';
 import {render} from 'react-dom';
-import {hashHistory} from 'react-router';
+import history from './history';
 import configureStore from './store/configureStore';
 import Root from './views/Root';
 
 const store = configureStore();
-const node = <Root history={hashHistory} store={store} />;
+const node = <Root history={history} store={store} />;
 const $target = document.getElementById('mount');
 
 render(node, $target);
